ff65f327e94888e1858f18c0801f0d8e87ce1668
Malik <malikalghneimin@gmail.com>
2025-12-06 00:19:49 -0500

    Day 3 part 1 added - 2025

---
+++ 2025/Day3/solutionPart1.py
@@ -0,0 +1,26 @@
+# Open the input file and read a line.
+file = open("2025/Day3/input.txt")
+line = file.readline()
+
+# Variables to record the maximun and sum values.
+max = 0
+sum = 0
+
+# While loop to go through every line in the input file.
+while line:
+    # First for loop for the first digit.
+    for i in range(0, len(line) - 1):
+        first = int(line[i])
+        # Second for loop for the second digit.
+        for j in range(i + 1, len(line) - 1):
+            second = int(line[j])
+            # Compare to the max value if there is any.
+            if (first * 10 + second) > max:
+                max = first * 10 + second
+    # Increment the maximum value found in the line to the sum.
+    sum += max
+    max = 0 # Reset maximum value.
+    line = file.readline()
+
+print(sum)
+    
